2e7bc9760ab4d45cb44fa79e502b21842a351e9b
eder antonio <antonioeder007@gmail.com>
2023-08-05 10:08:31 -0400

    refactor: Refatorar para SOLID

--- back/api.py
+++ back/api.py
@@ -1,17 +1,36 @@
-#cmd: uvicorn api:app --reload
 
 import os
 import json
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from deepface import DeepFace
+from deepface.commons import functions
+from typing import List
 import numpy as np
+
+class DeepFaceAnalyzer:
+    def analyze(self, image_path: str) -> dict:
+        resultado = DeepFace.analyze(image_path, actions=("age", "emotion", "gender", "race"), detector_backend='mtcnn')
+        os.remove(image_path)
+        return resultado
+
+class FaceVerifier:
+    def verify(self, image_path1: str, image_path2: str) -> dict:
+        resultado = DeepFace.verify(img1_path=image_path1, img2_path=image_path2, model_name='Facenet')
+        os.remove(image_path1)
+        os.remove(image_path2)
+        return resultado
+
+class JsonSerializable:
+    def serialize(self, obj):
+        if isinstance(obj, np.bool_):
+            return bool(obj)
+        raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
 
 def serialize(obj):
     if isinstance(obj, np.bool_):
         return bool(obj)
     raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
-
 
 app = FastAPI()
 app.add_middleware(
@@ -19,32 +38,35 @@ app.add_middleware(
     allow_origins=['*']
 )
 
+deep_face_analyzer = DeepFaceAnalyzer()
+face_verifier = FaceVerifier()
+json_serializer = JsonSerializable()
+
 @app.get("/")
 def hello_root():
     return {"message": "Bem vindo"}
- 
+
 @app.post("/analyze")
 async def analyze_face(file: UploadFile = File(...)):
-
-    with open(file.filename, "wb") as f:
+    image_path = file.filename
+    with open(image_path, "wb") as f:
         f.write(await file.read())
-        resultado = DeepFace.analyze(file.filename, actions=("age", "emotion", "gender", "race"), detector_backend='mtcnn')
-        os.remove(file.filename)
-        
+    resultado = deep_face_analyzer.analyze(image_path)
     return resultado
 
 @app.post("/verify")
 async def analyze_face(image1: UploadFile = File(...), image2: UploadFile = File(...)):
-    with open(image1.filename, "wb") as f1:
+    image_path1 = image1.filename
+    image_path2 = image2.filename
+
+    with open(image_path1, "wb") as f1:
         f1.write(await image1.read())
 
-    with open(image2.filename, "wb") as f2:
+    with open(image_path2, "wb") as f2:
         f2.write(await image2.read())
 
-    resultado = DeepFace.verify(img1_path = image1.filename, img2_path = image2.filename, model_name='Facenet')
-
-    os.remove(image1.filename)
-    os.remove(image2.filename)
+    resultado = face_verifier.verify(image_path1, image_path2)
+    print(resultado)
 
     json_string = json.dumps(resultado, default=serialize)
     json_object = json.loads(json_string)
